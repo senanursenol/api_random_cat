24a2c57b2dcb836da95f26b5fcbfcc6557473409
unknown <senanursenol4@gmail.com>
2025-03-27 22:04:50 +0300

    lk proje yüklemesi

--- script/script.js
+++ script/script.js
@@ -1,20 +1,3 @@
-async function fetchCat() {
-    try {
-        const response = await fetch("https://api.thecatapi.com/v1/breeds");
-        const data = await response.json();
-        
-        // Rastgele bir kedi türü seç
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const cat = data[randomIndex];
-
-        document.getElementById("cat-img").src = `https://cdn2.thecatapi.com/images/${cat.reference_image_id}.jpg`;
-        document.getElementById("cat-name").textContent = cat.name;
-        document.getElementById("cat-lifespan").textContent = cat.life_span;
-        document.getElementById("cat-description").textContent = cat.description;
-    } catch (error) {
-        console.error("API çağrısı başarısız:", error);
-    }
-}
 
 // Sayfa yüklendiğinde bir kedi getir
 fetchCat();
